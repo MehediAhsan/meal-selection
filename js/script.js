87fb5c8dfbc1eb76b882653fd4ffa4a376b972f3
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-29 14:46:37 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 const loadMeal = (search) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-    .then(res => res.json())
-    .then(data => displaymeal(data.meals))
+        .then(res => res.json())
+        .then(data => displaymeal(data.meals))
 }
 const displaymeal = meals => {
     const mealsContainer = document.getElementById('meals-container');
     mealsContainer.innerHTML = ``;
-    for(const meal of meals){
+    for (const meal of meals) {
         // console.log(meal)
         const mealDiv = document.createElement('div');
-        mealDiv.classList.add('card','card-compact','bg-base-100','shadow-xl')
+        mealDiv.classList.add('card', 'card-compact', 'bg-base-100', 'shadow-xl')
         mealDiv.innerHTML = `
         <figure><img class="h-44 w-full" src="${meal.strMealThumb}" alt="Shoes" /></figure>
         <div class="card-body">
         <h2 class="card-title">${meal.strMeal}</h2>
         <p>${meal.strInstructions.slice(0, 100)}</p>
         <div class="card-actions justify-end">
-            <button onclick = "loadMealDetail(${meal.idMeal})" class="buy-btn btn btn-primary">Details</button>
+            <label onclick = "loadMealDetail(${meal.idMeal})" for="my-modal-4" class="btn modal-button btn-primary">Details</label>
         </div>
         </div>
         `;
@@ -26,8 +26,30 @@ const displaymeal = meals => {
 
 const loadMealDetail = (code) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${code}`)
-    .then(res => res.json())
-    .then(data => console.log(data.meals))
+        .then(res => res.json())
+        .then(data => displaymealDetail(data.meals))
+}
+
+const displaymealDetail = meals => {
+    const mealDetail = document.getElementById('meal-detail');
+    for (const meal of meals){
+        console.log(meal)
+    mealDetail.innerHTML = `
+    <label class="modal-box relative" for="">
+    <div class="flex">
+    <img class="h-64 w-52 mr-5" src="${meal.strMealThumb}" alt="Shoes" />
+    <div>
+    <h3 class="text-lg font-semibold text-white mb-3">Food Name: ${meal.strMeal}</h3>
+    <p>Food Category: ${meal.strCategory}</p>
+    <p class="mb-3">Food Category: ${meal.strArea}</p>
+    <p>Details: ${meal.strInstructions.slice(0, 100)}</p>
+    <div class="flex justify-end"><button class="btn btn-active btn-primary mt-3 ">Buy Now</button></div>
+    
+    </div>
+    </div>
+    </label>
+    `       
+    }
 }
 
 const searchMeal = () => {
